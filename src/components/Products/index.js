5ba03b9c2b1c9fdd40a00011110aa2c0a759cb27
Saiyeesh <saiyeesh7941@gmail.com>
2023-11-20 04:53:06 +0000

    solution

---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import Header from '../Header'
+import AllProducts from '../AllProducts'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <AllProducts />
+  </>
+)
+
+export default Products
